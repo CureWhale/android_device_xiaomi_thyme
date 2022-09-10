93f430ab7206a78a04cb5ab75f47f8ae61738be7
CureWhale <97373469+CureWhale@users.noreply.github.com>
2022-09-10 16:34:24 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_thyme.mk
+    $(LOCAL_DIR)/dot_thyme.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_thyme-eng \
-    aosp_thyme-userdebug \
-    aosp_thyme-user
+    dot_thyme-eng \
+    dot_thyme-userdebug \
+    dot_thyme-user
